e2b9088993b1c48768a8eafb4e53ab3a8534cfa5
Sofilus <sofia00olsson@gmail.com>
2023-03-07 16:41:39 +0100

    Made it possible to add recipe to json file with postman

---
+++ routes/recept.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+const fs = require('fs');
+
+router.post('/', function(req, res) {
+  fs.readFile("./recept.json", function (err, data) {
+    if(err){
+      console.log(err);
+    }
+
+    const recepies = JSON.parse(data)
+
+    let recept = req.body
+
+    recepies.push(recept);
+
+    fs.writeFile("./recept.json", JSON.stringify(recepies), function(err){
+      if(err){
+        console.log(err)
+      }
+    })
+    res.send("Worked")
+    return;
+  })
+});
+
+module.exports = router;
